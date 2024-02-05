4cf3fb5cc45f69dcb6ad632e51b3a7a7bd1905df
adichak27 <adichak2002@gmail.com>
2024-02-05 12:25:07 -0500

    login screen configured

--- app/src/main/java/com/cs4520/assignment1/LoginFragment.kt
+++ app/src/main/java/com/cs4520/assignment1/LoginFragment.kt
@@ -1,4 +1,42 @@
 package com.cs4520.assignment1
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 
-class LoginFragment {
+class LoginFragment : Fragment() {
+    private lateinit var usernameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.main_activity, container, false)
+        usernameEditText = view.findViewById(R.id.username)
+        passwordEditText = view.findViewById(R.id.password)
+        loginButton = view.findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (username == "admin" && password == "admin") {
+                usernameEditText.text.clear();
+                passwordEditText.text.clear();
+
+                //findNavController().navigate(R.id.action_loginFragment_to_productListFragment)
+            } else {
+                Toast.makeText(activity, "Incorrect username or password", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return view
+    }
 }
